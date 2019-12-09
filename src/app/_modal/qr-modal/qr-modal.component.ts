54f62c463f856a656e86100a98bd061d11b89eb8
EvgenOksigen <e.dedenev@gmail.com>
2019-12-09 23:15:51 +0200

    :add material modal window and cactomize

--- src/app/_modal/qr-modal/qr-modal.component.ts
+++ src/app/_modal/qr-modal/qr-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material'
+
 
 @Component({
     selector: 'qr-modal',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrModal implements OnInit {
 
-    constructor() { }
+    constructor(private dialog: MatDialog) { }
 
     ngOnInit() {
     }
-
+    onClose() {
+        this.dialog.closeAll();
+    }
 }
